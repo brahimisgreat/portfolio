de7b0b22ba6ae4f369ab974d0a314cf276acd676
brahim coulibaly <brahimiscoding@gmail.com>
2024-08-09 20:12:16 -0400

    first

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    const [count, setCount] = useState(0)
+  
+    return (
+      <header className="header">
+
+      
+      </header>
+        
+       
+    )
+  }
+  
+  export default App
+  
---
+++ src/Components/Pages/SinglePage.jsx
@@ -0,0 +1,13 @@
+function SinglePage() {
+    const [count, setCount] = useState(0)
+  
+    return (
+      <div className="singlePage">
+        
+      </div>
+       
+    )
+  }
+  
+  export default SinglePage
+  
