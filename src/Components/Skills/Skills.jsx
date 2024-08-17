e80496747639830c3c3884c09e3bc12dbbc656a5
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-08-17 01:46:14 -0400

    gfdg

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,32 +1,34 @@
-import './Skills.css'
+import "./Skills.css";
+import skillsBG from "../../assets/images/pattern-rings.svg";
 
-export const Skills = () => {
+export default function Skills() {
   return (
     <div className="skills">
-        <div className='nameOfSkill'>
-            <h4>HTML</h4>
-            <p>4 Years Experience</p>
-        </div>
-        <div className='nameOfSkill'>
-            <h4>CSS</h4>
-            <p>4 Years Experience</p>
-        </div>
-        <div className='nameOfSkill'>
-            <h4>Javascript</h4>
-            <p>4 Years Experience</p>
-        </div>
-        <div className='nameOfSkill'>
-            <h4>Accessibility</h4>
-            <p>4 Years Experience</p>
-        </div>
-        <div className='nameOfSkill'>
-            <h4>React</h4>
-            <p>3 Years Experience</p>
-        </div>
-        <div className='nameOfSkill'>
-            <h4>Sass</h4>
-            <p>3 Years Experience</p>
-        </div>
+      <div className="nameOfSkill">
+        <h4>HTML</h4>
+        <p>4 Years Experience</p>
+      </div>
+      <div className="nameOfSkill">
+        <h4>CSS</h4>
+        <p>4 Years Experience</p>
+      </div>
+      <div className="nameOfSkill">
+        <h4>Javascript</h4>
+        <p>4 Years Experience</p>
+      </div>
+      <div className="nameOfSkill">
+        <h4>Accessibility</h4>
+        <p>4 Years Experience</p>
+      </div>
+      <div className="nameOfSkill">
+        <h4>React</h4>
+        <p>3 Years Experience</p>
+      </div>
+      <div className="nameOfSkill">
+        <h4>Sass</h4>
+        <p>3 Years Experience</p>
+      </div>
+      <img src={skillsBG} alt="background decoration image" className="skillsBG" />
     </div>
-  )
+  );
 }
