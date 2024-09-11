edf008bedfea14ef30ab0b9be004407c24814e8f
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-09-10 21:36:58 -0400

    fdf

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import { Header } from './Components/Header'
 import { Intro } from './Components/Pages/Intro'
 import { Routes, Route } from 'react-router-dom'
+import { Skills } from './Components/Pages/Skills'
 
 function App() {
 
   return (
-    <div className="app">
+    <div className="app flex flex-col items-center gap-16 ">
       <Header />
       <Routes>
         <Route path='/' element={<Intro />} />
       </Routes>
+      <Skills />
     </div>
      
   )
---
+++ src/Components/Pages/Skills.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './Skills.css'
+
+
+export const Skills = () => {
+  return (
+    <div className='skills  flex flex-col flex-wrap gap-10'>
+        <div className='languages-container'>
+            <h1>HTML</h1>
+            <p>3 Years Experience</p>
+        </div>
+        <div className='languages-container '>
+            <h1>CSS</h1>
+            <p>3 Years Experience</p>
+        </div>
+        <div className='languages-container'>
+            <h1>JavaScript</h1>
+            <p>2 Years Experience</p>
+        </div>
+        <div className='languages-container'>
+            <h1>React</h1>
+            <p>1 Year Experience</p>
+        </div>
+        <div className='languages-container'>
+            <h1>node js</h1>
+            <p>1 Year Experience</p>
+        </div>
+        <div className='languages-container  text-center'>
+            <h1>MySQL</h1>
+            <p>1 Year Experience</p>
+        </div>
+        <div className='languages-container'>
+            <h1>ExpressJs</h1>
+            <p>1 Year Experience</p>
+        </div>
+        <div className='languages-container'>
+            <h1>Sass</h1>
+            <p>1 Year Experience</p>
+        </div>
+        <div className='languages-container'>
+            <h1>TailwindCss</h1>
+            <p>1 Year Experience</p>
+        </div>
+        <div className='languages-container'>
+            <h1>Bootstrap</h1>
+            <p>1 Year Experience</p>
+        </div>
+        <div className='languages-container'>
+            <h1>Typescript</h1>
+            <p>1 Year Experience</p>
+        </div>
+    </div>
+  )
+}
