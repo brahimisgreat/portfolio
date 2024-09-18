4c06fe06f0955d6199dc5148dc267fb162dee96d
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2024-09-17 20:35:28 -0400

    fd

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -1,30 +1,28 @@
-import React from "react";
 import frontEnd from "../../assets/images/icon-frontend-mentor.svg";
 import github from "../../assets/images/icon-github.svg";
 import linkedin from "../../assets/images/icon-linkedin.svg";
+import "./Contact.css";
 
 export default function Contact() {
   return (
-    <div className="w-full">
-    <form>
+    <div className="contact  flex flex-col items-center justify-center ">
+  
+    <form className="flex flex-col items-center w-11/12">
       <filedset>
         <legend className="text-4xl font-bold text-center ">Contact</legend>
         <p>I would love to hear about your project and how I could help.
          Please fill in the form, and I’ll get back to you as soon as possible.
          </p>
-         <p>
-
-          <input placeholder="NAME" />
+         <p >
+          <input  className="w-full bg-transparent h-11 border-b-2" placeholder="NAME" /> 
          </p>
          <p>
-          <input placeholder="EMAIL" type="email"/>
+          <input  className="w-full bg-transparent h-11 border-b-2" placeholder="EMAIL" type="email"/>
          </p>
          <p>
-          
-          <textarea  placeholder="MESSAGE"/>
+          <textarea   className="w-full bg-transparent h-28 border-b-2 py-2" placeholder="MESSAGE"/>
          </p>
-
-
+         <button>SEND MESSAGE</button>
       </filedset>
 
     </form>
